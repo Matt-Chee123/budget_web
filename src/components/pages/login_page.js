25324e10e1c4e0ef43f61a7a4cb98fd332be51ad
Matt-Chee123 <cheematthew10@gmail.com>
2025-01-12 16:54:29 +0000

    made background dark and text white

--- src/components/pages/login_page.js
+++ src/components/pages/login_page.js
@@ -11,14 +11,10 @@ function LoginPage() {
         <body class="h-full">
         ```
       */}
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="container h-full">
+            <div className="flex flex-1 h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
-                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">
                         Sign in to your account
                     </h2>
                 </div>
@@ -26,7 +22,7 @@ function LoginPage() {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form action="#" method="POST" className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                            <label htmlFor="email" className="block text-sm/6 font-medium">
                                 Email address
                             </label>
                             <div className="mt-2">
@@ -36,14 +32,14 @@ function LoginPage() {
                                     type="email"
                                     required
                                     autoComplete="email"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                <label htmlFor="password" className="block text-sm/6 font-medium">
                                     Password
                                 </label>
                                 <div className="text-sm">
@@ -59,7 +55,7 @@ function LoginPage() {
                                     type="password"
                                     required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
@@ -74,6 +70,7 @@ function LoginPage() {
                         </div>
                     </form>
                 </div>
+            </div>
             </div>
         </>
     )
