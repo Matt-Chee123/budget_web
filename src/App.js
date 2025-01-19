27b79d6ca6dd0e65c31009dc167ee6973167fee1
Matt-Chee123 <cheematthew10@gmail.com>
2025-01-19 20:06:12 +0000

    setup login code with axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./config/app_routing"
-import { AxiosProvider } from './contexts/axiosContext';  // Import AxiosProvider
 
 
 function App() {
   return (
-    <AxiosProvider>
+
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
-    </AxiosProvider>
   );
 }
 
--- src/components/pages/login_page.js
+++ src/components/pages/login_page.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import Axios from "axios";
-
+import axiosInstance from '../../config/axios';
 function LoginPage() {
     let [username, setUsername] = useState("");
     let [password, setPassword] = useState("");
     let [error, setError] = useState("");
 
+    const handleLogin = async  (e) => {
+        console.log("login");
+        e.preventDefault();
+        console.log(e)
+        try {
+            const response = await axiosInstance.post("/users/login/", { username, password });
+            console.log(response);
+        } catch (error) {
+            setError('Login failed');
+        }
+    }
     return (
         <>
-            {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
             <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8 align-baseline mt-20">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">
@@ -24,19 +26,20 @@ function LoginPage() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form onSubmit={handleLogin} method="POST" className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm/6 font-medium">
-                                Email address
+                            <label htmlFor="username" className="block text-sm/6 font-medium">
+                                Username
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
+                                    onChange={(e) => { setUsername(e.target.value); }}
+                                    id="username"
+                                    name="username"
+                                    type="username"
                                     required
-                                    autoComplete="email"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    autoComplete="username"
+                                    className="block w-full rounded-md text-black px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
@@ -54,12 +57,13 @@ function LoginPage() {
                             </div>
                             <div className="mt-2">
                                 <input
+                                    onChange={(e) => { setPassword(e.target.value); }}
                                     id="password"
                                     name="password"
                                     type="password"
                                     required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    className="block w-full rounded-md text-black bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
