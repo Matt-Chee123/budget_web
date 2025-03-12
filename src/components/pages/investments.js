7234e834dc7e7fff864246d11d8550ed75073e3b
Matt-Chee123 <cheematthew10@gmail.com>
2025-03-12 20:43:42 +0000

    add bubbles to investment page

--- src/components/pages/investments.js
+++ src/components/pages/investments.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 import Box from '../layouts/box';
+import AccountSelection from "../charts/b_account_selector";
+import TotalBalance from "../charts/b_account_total_income";
 
 function Investments() {
     return (
-        <></>
-    )
+        <div className="flex flex-1 flex-col md:flex-row w-full">
+            <div className="basis-2/5 flex-col">
+                <div className="flex-col h-full">
+                    <div className="flex flex-row h-1/3">
+                        <div className="w-1/2 h-full p-3 pr-0">
+                            <Box>
+                            </Box>
+                        </div>
+                        <div className="w-1/2 h-full p-3">
+                            <Box>
+                            </Box>
+                        </div>
+                    </div>
+                    <div className="h-2/3 p-3 pt-0 pb-20">
+                        <Box></Box>
+                    </div>
+                </div>
+            </div>
+            <div className="basis-3/5">
+                <div className="flex-col h-screen p-3 pl-0 pb-20">
+                    <Box></Box>
+                </div>
+            </div>
+        </div>)
 }
 
 export default Investments;
